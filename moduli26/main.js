f6806ee43e8e54d3a7e8a0d6f94b465908681f3f
detjonkrasniqi <detjonkrasniqi6@gmail.com>
2026-03-05 17:20:11 +0100

    moduli 26

--- moduli26/main.js
+++ moduli26/main.js
@@ -1,1 +1,68 @@
 console.log("Helloooo")
+
+var x = 10;
+
+var value = "digjital School";
+
+var y =20;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+var a =99;
+
+console.log(a);
+
+a++;
+
+console.log(b);
+
+var b=201
+
+b--;
+
+var c=500;
+
+c -=54;
+
+console.log(c);
+
+var d =45;
+
+d+=54;
+console.log(d);
+
+var e =354;
+
+e*=2;
+
+console.log(e);
+
+
+var v = 100;
+
+
+var w = 50;
+
+
+// operatort e krahasimit
+
+
+console.log(v == w);
+
+
+console.log(v != w);
+
+
+console.log(v > w);
+
+
+console.log(v < w);
+
+
+
