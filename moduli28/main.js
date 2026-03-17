bdcb46003513d69c46d3866e6900d7c25c115050
detjonkrasniqi <detjonkrasniqi6@gmail.com>
2026-03-17 17:48:32 +0100

    moduli28

---
+++ moduli28/main.js
@@ -0,0 +1,37 @@
+var button1=document.getElementById("btn1");
+
+
+button1.onclick=function(){
+    alert("other button was clicked")
+}
+
+
+var button2=document.getElementById("btn2");
+
+
+button2.onmouseover=function(){
+    alert("Mouse is over");
+}
+var button3=document.getElementById("btn3");
+
+
+button3.onmouseleave=function(){
+    alert("mouse left")
+
+}
+
+var button4=document.getElementById("btn4");
+
+
+button4.onmousewheel=function(){
+    alert("mouse wheel")
+
+}
+
+var v_button = document.getElementById("btn6");
+
+var v_txt = document.getElementById("txt1");
+v_button.onclick = function(){
+ v_text.style.color ="red"  
+}
+  
