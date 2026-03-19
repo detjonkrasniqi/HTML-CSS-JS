136dd0e5c22207d6569e731bb6cd8b768abfbcc2
detjonkrasniqi <detjonkrasniqi6@gmail.com>
2026-03-19 17:45:36 +0100

    moduli28

--- moduli28/main.js
+++ moduli28/main.js
@@ -34,4 +34,36 @@ var v_txt = document.getElementById("txt1");
 v_button.onclick = function(){
  v_text.style.color ="red"  
 }
+
+
+var button1=document.getElementById("btn11");
+var teksti = document.getElementById("txt");
+
+var button22=document.getElementById("btn22");
+var teksti1 = document.getElementById("txt1");
+
+
+
+button1.onclick = function(){
+    teksti.style.color = 'red'
+    teksti.style.backgroundColor = 'lightgrey'
+    teksti.style.fontSize = '100px'
+    teksti.style.padding = '20px'
+    teksti.style.textAlign = 'center'
+}
+
+button22.onclick = function(){
+    teksti1.style.cssText= 'color:blue; background-color:lightgrey; font-size:25px; font-family:fantasy'
+}
+
+var ifundit = document.getElementById('ifundit')
+var pg = document.getElementById('pg')
+
+ifundit.onclick = function(){
+    pg.setAttribute("class","paragrafi")
+}
+
+
+
+
   
