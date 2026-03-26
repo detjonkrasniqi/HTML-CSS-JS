92c752140df3bfdbb464193b134d981a719ce177
detjonkrasniqi <detjonkrasniqi6@gmail.com>
2026-03-26 17:46:27 +0100

    moduli30

---
+++ moduli30/main.js
@@ -0,0 +1,20 @@
+// var mosha =7;
+
+// if(mosha <8){
+//     document.write("Your child is to young to learn programming");
+// }else{
+//     document.write("We will call you soon to let you know for the groups");
+// }
+
+var age = 20;
+
+if(age < 8){
+    document.write("Your child is to young to learn programming");  
+}else if( age>18){
+    document.write("Your child is to young to learn programming");
+}else{
+    document.write("We will call you soon to let you know for the groups");
+}
+
+
+
