77ce04c32cfd846e9c0b1e508f6ebc61c0e07bcd
detjonkrasniqi <detjonkrasniqi6@gmail.com>
2026-04-02 17:43:57 +0200

    moduli30

---
+++ moduli30/DSClock/img/js/main.js
@@ -0,0 +1,23 @@
+var wakeuptime;
+var dstime;
+var sleeptime;
+var noon = 12;
+
+function showCurrentTime() {
+    var clock = document.getElementById('clock');
+    var currentTime = new Date();
+
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    var meridian = "AM";
+
+    if (hours >= noon) {
+        meridian = "PM";
+    }
+
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian;
+    clock.innerHTML = clockTime;
+    changeImage();
+} 
